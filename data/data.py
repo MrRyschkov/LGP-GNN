ae232e921f267ea8baa48965688f42126631bb26
LGP-GNN-2021 <78425504+LGP-GNN-2021@users.noreply.github.com>
2021-02-09 09:55:01 +0100

    Update data.py

--- data/data.py
+++ data/data.py
@@ -1,13 +1,9 @@
 """
     File to load dataset based on user control from main file
 """
-from data.superpixels import SuperPixDataset
 from data.molecules import MoleculeDataset
-from data.TUs import TUsDataset
 from data.SBMs import SBMsDataset
-from data.TSP import TSPDataset
 from data.COLLAB import COLLABDataset
-from data.CSL import CSLDataset
 
 
 def LoadData(DATASET_NAME):
