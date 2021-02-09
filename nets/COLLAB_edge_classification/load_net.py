2b655737534e8957c8a6ba7e9fe4aedd4f9bbc5b
LGP-GNN-2021 <78425504+LGP-GNN-2021@users.noreply.github.com>
2021-02-09 10:16:45 +0100

    Update load_net.py

--- nets/COLLAB_edge_classification/load_net.py
+++ nets/COLLAB_edge_classification/load_net.py
@@ -7,10 +7,8 @@ from nets.COLLAB_edge_classification.gated_gcn_net import GatedGCNNet
 from nets.COLLAB_edge_classification.gcn_net import GCNNet
 from nets.COLLAB_edge_classification.gat_net import GATNet
 from nets.COLLAB_edge_classification.graphsage_net import GraphSageNet
-from nets.COLLAB_edge_classification.gin_net import GINNet
 from nets.COLLAB_edge_classification.mo_net import MoNet as MoNet_
 from nets.COLLAB_edge_classification.mlp_net import MLPNet
-from nets.COLLAB_edge_classification.matrix_factorization import MatrixFactorization
 
 
 def GatedGCN(net_params):
@@ -25,8 +23,6 @@ def GAT(net_params):
 def GraphSage(net_params):
     return GraphSageNet(net_params)
 
-def GIN(net_params):
-    return GINNet(net_params)
 
 def MoNet(net_params):
     return MoNet_(net_params)
@@ -34,8 +30,6 @@ def MoNet(net_params):
 def MLP(net_params):
     return MLPNet(net_params)
 
-def MF(net_params):
-    return MatrixFactorization(net_params)
 
 def gnn_model(MODEL_NAME, net_params):
     models = {
@@ -43,10 +37,8 @@ def gnn_model(MODEL_NAME, net_params):
         'GCN': GCN,
         'GAT': GAT,
         'GraphSage': GraphSage,
-        'GIN': GIN,
         'MoNet': MoNet,
         'MLP': MLP,
-        'MF': MF,
     }
         
     return models[MODEL_NAME](net_params)
